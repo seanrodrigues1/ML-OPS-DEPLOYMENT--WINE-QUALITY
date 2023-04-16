9eebf69bfcedb75afe832bdf9858771f0350507a
seanrodrigues1 <seanrodrigues12@gmail.com>
2023-04-16 17:37:13 -0400

    created get_data file

--- src/get_data.py
+++ src/get_data.py
@@ -13,7 +13,7 @@ def get_data(config_path):
     # print(config)
     data_path = config["data_source"]["s3_source"]
     df = pd.read_csv(data_path, sep=",", encoding='utf-8')
-    return df
+    return df        #use print if you wanna view the df using command line using python src/get_data.py
 
 
 
@@ -21,4 +21,4 @@ if __name__=="__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
-    data = get_data(config_path=parsed_args.config)
+    data=get_data(config_path=parsed_args.config)
