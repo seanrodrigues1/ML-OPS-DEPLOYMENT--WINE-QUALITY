d74c66a00a261c4fa1cfa5f647647745c2151f28
seanrodrigues1 <seanrodrigues12@gmail.com>
2023-04-25 17:05:28 -0400

    added host and port no. in app.py

--- app.py
+++ app.py
@@ -28,4 +28,4 @@ def predict():
     return render_template("index.html",prediction_text="The wine quality is {}".format(output))
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
